01a40ef1259e2f020efaa911edcd2198fce509ec
Gibeom Choi <kcdevdes@gmail.com>
2024-11-09 11:02:55 -0500

    add: upload file through multer

--- backend/routes/submit-router.js
+++ backend/routes/submit-router.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const {hello} = require("../services/submit-service");
+const {hello, upload, handleUpload} = require("../services/submit-service");
 const router = express.Router()
 
 router.get('/', hello);
+// router.post('/file', upload.single('class_file'), handleUpload);
+router.post('/file', upload.single('class_file'), handleUpload);
 
 module.exports = router;
