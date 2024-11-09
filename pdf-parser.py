de21c827deb46ca1f2941aeaf7249264102a39eb
Daniel Chin <djchin2004@gmail.com>
2024-11-09 02:20:57 -0500

    add: multiple pages in pd fparser

--- pdf-parser.py
+++ pdf-parser.py
@@ -2,8 +2,12 @@ from pypdf import PdfReader
 
 def extract_text(pdf_file):
     reader = PdfReader(pdf_file)
-    page = reader.pages[0]
-    txt = page.extract_text()
-    print(txt)    
-    
-extract_text("deeznuts.pdf")
+    ## need to iterate through all pages of pdf input, extract text from each page
+    text = ""
+    for i in range(len(reader.pages)):
+        page = reader.pages[i]
+        text += page.extract_text()
+    print(text)
+def write_text(text):
+    with open("output.txt", "w") as file:
+        file.write(text)
