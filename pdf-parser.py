6f787bb3b54a24e3ab17ecd7ff6df0787e4c663e
Daniel Chin <djchin2004@gmail.com>
2024-11-09 02:23:11 -0500

    updated test for pdf parser

--- pdf-parser.py
+++ pdf-parser.py
@@ -4,10 +4,13 @@ def extract_text(pdf_file):
     reader = PdfReader(pdf_file)
     ## need to iterate through all pages of pdf input, extract text from each page
     text = ""
-    for i in range(len(reader.pages)):
+    for i in range(len(reader.pages) - 1): 
         page = reader.pages[i]
         text += page.extract_text()
     print(text)
+    
+extract_text("deeznuts.pdf")
+
 def write_text(text):
     with open("output.txt", "w") as file:
         file.write(text)
