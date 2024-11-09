dbee904148a0e3eef190b8527b5a482169373ca3
Daniel Chin <djchin2004@gmail.com>
2024-11-09 15:08:28 -0500

    add: dot animation

--- frontend/src/page/Feedback/Feedback.js
+++ frontend/src/page/Feedback/Feedback.js
@@ -1,23 +1,9 @@
-import logo from '../../logo.svg';
 import '../../App.css';
 
 function Feedback() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>Feedback.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"a
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="feedback-page">
+            <div className="feedback-container"></div>
         </div>
     );
 }
