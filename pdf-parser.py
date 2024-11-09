acaf11508b41375a8a51f8897aa8b89762660f3d
Daniel Chin <djchin2004@gmail.com>
2024-11-09 02:25:10 -0500

    updated pdf parser test

--- pdf-parser.py
+++ pdf-parser.py
@@ -4,7 +4,7 @@ def extract_text(pdf_file):
     reader = PdfReader(pdf_file)
     ## need to iterate through all pages of pdf input, extract text from each page
     text = ""
-    for i in range(len(reader.pages) - 1): 
+    for i in range(len(reader.pages)): 
         page = reader.pages[i]
         text += page.extract_text()
     print(text)
